77a210bf3f86480bf8da4d54e5151013c1dacea8
balazsDombovari <dbalazs1219@gmail.com>
2019-08-24 19:16:16 +0200

    first commit

---
+++ src/com/codecool/dombi/oodesign/Main.java
@@ -0,0 +1,8 @@
+package com.codecool.dombi.oodesign;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("A good practice for OOP design patterns.");
+    }
+}
---
+++ src/com/codecool/dombi/oodesign/creational/design/patterns/Singleton.java
@@ -0,0 +1,24 @@
+package com.codecool.dombi.oodesign.creational.design.patterns;
+
+/**
+ * When to use:
+ *     Singleton pattern should be used when we must ensure that only
+ *      one instance of a class is created
+ *      and when the instance must be available through all the code.
+ *     A special care should be taken in multi-threading environments when multiple threads must access
+ *      the same resources through the same singleton object.
+ */
+
+
+/**
+ * Common Usage:
+ * There are many common situations when singleton pattern is used:
+ * - Logger Classes
+ * - Configuration Classes
+ * - Accesing resources in shared mode
+ * - Other design patterns implemented as Singletons: Factories and Abstract Factories,
+ *   Builder, Prototype
+ */
+
+public class Singleton {
+}
